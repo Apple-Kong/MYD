46cc722647cb670f0cbcb3616989dd229635b363
Apple-Kong <kongthn@gmail.com>
2022-05-01 14:08:29 +0900

    Initial Commit

---
+++ MYD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MYD
+//
+//  Created by GOngTAE on 2022/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MYD/MYDApp.swift
@@ -0,0 +1,17 @@
+//
+//  MYDApp.swift
+//  MYD
+//
+//  Created by GOngTAE on 2022/05/01.
+//
+
+import SwiftUI
+
+@main
+struct MYDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
