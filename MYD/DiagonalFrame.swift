1029729ab6392324909b2afe5bc5cb7537378239
Apple-Kong <kongthn@gmail.com>
2022-05-01 18:14:28 +0900

    music grid 완성

---
+++ MYD/DiagonalFrame.swift
@@ -0,0 +1,51 @@
+//
+//  DiagonalAppearance.swift
+//  MYD
+//
+//  Created by GOngTAE on 2022/05/01.
+//
+
+import SwiftUI
+
+
+
+struct DiagonalFrame: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: 200))
+        // 2.
+        path.addLine(to: CGPoint(x: 100, y: 0))
+        // 3.
+        path.addLine(to: CGPoint(x: 300, y: 0))
+        
+        path.addLine(to: CGPoint(x: 300, y: 300))
+        
+        path.addLine(to: CGPoint(x: 0, y: 300))
+        
+        
+        //path 를 닫아주어야함.
+        path.closeSubpath()
+        
+        return path
+    }
+}
+
+struct DiagonalFrame_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            DiagonalFrame()
+                
+            
+            
+            Image("boombap")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipShape(
+                    DiagonalFrame()
+                )
+                .frame(width: 300, height: 300)
+                .previewLayout(.sizeThatFits)
+        }
+    }
+}
--- MYD/MusicButtonView.swift
+++ MYD/MusicButtonView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct MusicButtonView: View {
-
+    
     var text: String
+    
+    @Binding var isSelected: Bool
     
     
     var body: some View {
@@ -19,6 +21,11 @@ struct MusicButtonView: View {
             
             //이미지 트림 배치하기
             
+            Image("boombap")
+                .resizable()
+                .clipShape(
+                    DiagonalFrame()
+                )
             
             VStack {
                 HStack {
@@ -35,13 +42,35 @@ struct MusicButtonView: View {
         }
         .aspectRatio(16 / 14, contentMode: .fit)
         .cornerRadius(10)
+        .if(isSelected, transform: { view in
+            view.overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color("AccentColor"), lineWidth: 2.5)
+            )
+        })
     }
 }
+
+extension View {
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
+
 
 struct MusicButtonView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            MusicButtonView(text: "R&B")
+            MusicButtonView(text: "R&B", isSelected: .constant(false))
+                .frame(height: 140)
+                .previewLayout(.sizeThatFits)
+            
+            
+            MusicButtonView(text: "R&B", isSelected: .constant(true))
                 .frame(height: 140)
                 .previewLayout(.sizeThatFits)
             
@@ -51,17 +80,21 @@ struct MusicButtonView_Previews: PreviewProvider {
 }
 
 struct MusicGrid: View {
+    
+    //간격 한번에 설정하기 위한 변수
+    var totalSpacing: CGFloat = 14
+    
     var body: some View {
         ScrollView {
             LazyVGrid(columns: [
-                GridItem(.flexible(), spacing: 20),
+                GridItem(.flexible(), spacing: totalSpacing),
                 GridItem(.flexible())
-            ], spacing: 20) {
+            ], spacing: totalSpacing) {
                 ForEach(0..<20) { item in
-                    MusicButtonView(text: "R&B")
+                    MusicButtonView(text: "R&B", isSelected: .constant(false))
                 }
             }
-        .padding(.horizontal, 20)
+        .padding(.horizontal, totalSpacing)
         }
     }
 }
